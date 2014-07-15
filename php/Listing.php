3cbdd2c6924ac722d83013a598eb9e7795523c26
jamesl1001 <jamesl1001@msn.com>
2014-07-15 21:58:43 +0100

    Display listings depending upon Profile criteria

--- php/Listing.php
+++ php/Listing.php
@@ -4,7 +4,19 @@ class Listing {
     static function getListings() {
         require('db.php');
 
-        $sth = $dbh->query("SELECT propertyId, location, address, distanceUKC, distanceCCCU, distanceUKM, price, roomType, availableFrom, furnishing, timestamp FROM properties");
+        $userId = $_SESSION['s_userId'];
+
+        $sth = $dbh->query("SELECT lookingIn, roomType, availableFrom, minPrice, maxPrice FROM users WHERE userId=$userId");
+        $sth->setFetchMode(PDO::FETCH_OBJ);
+        $user = $sth->fetch();
+
+        if($user->roomType == 'any') {
+            $roomTypeQuery = '';
+        } else {
+            $roomTypeQuery = "AND roomType='$user->roomType'";
+        }
+
+        $sth = $dbh->query("SELECT propertyId, location, address, distanceUKC, distanceCCCU, distanceUKM, price, roomType, availableFrom, furnishing, timestamp FROM properties WHERE location='$user->lookingIn' AND price BETWEEN $user->minPrice AND $user->maxPrice AND availableFrom > STR_TO_DATE('$user->availableFrom', '%Y-%m-%d') $roomTypeQuery");
         $sth->setFetchMode(PDO::FETCH_OBJ);
         $results = $sth->fetchAll();
 
