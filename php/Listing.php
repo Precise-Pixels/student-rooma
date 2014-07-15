9d3909b32a84486a1fa97adc48a9ed40f5f64204
jamesl1001 <jamesl1001@msn.com>
2014-07-15 22:44:52 +0100

    >= availableFrom

--- php/Listing.php
+++ php/Listing.php
@@ -16,7 +16,7 @@ class Listing {
             $roomTypeQuery = "AND roomType='$user->roomType'";
         }
 
-        $sth = $dbh->query("SELECT propertyId, location, address, distanceUKC, distanceCCCU, distanceUKM, price, roomType, availableFrom, furnishing, timestamp FROM properties WHERE location='$user->lookingIn' AND price BETWEEN $user->minPrice AND $user->maxPrice AND availableFrom > STR_TO_DATE('$user->availableFrom', '%Y-%m-%d') $roomTypeQuery");
+        $sth = $dbh->query("SELECT propertyId, location, address, distanceUKC, distanceCCCU, distanceUKM, price, roomType, availableFrom, furnishing, timestamp FROM properties WHERE location='$user->lookingIn' AND price BETWEEN $user->minPrice AND $user->maxPrice AND availableFrom >= STR_TO_DATE('$user->availableFrom', '%Y-%m-%d') $roomTypeQuery");
         $sth->setFetchMode(PDO::FETCH_OBJ);
         $results = $sth->fetchAll();
 
