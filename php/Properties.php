0d15e389e9692bd1f71887bbf9f2e512c664ce49
jamesl1001 <jamesl1001@msn.com>
2014-07-21 22:33:45 +0100

    Don't show properties that have already been decided upon

--- php/Properties.php
+++ php/Properties.php
@@ -16,7 +16,13 @@ class Properties {
             $roomTypeQuery = "AND roomType='$user->roomType'";
         }
 
-        $sth = $dbh->query("SELECT propertyId, location, address, distanceUKC, distanceCCCU, distanceUKM, price, roomType, availableFrom, furnishing, timestamp FROM properties WHERE location='$user->lookingIn' AND price BETWEEN $user->minPrice AND $user->maxPrice AND availableFrom >= STR_TO_DATE('$user->availableFrom', '%Y-%m-%d') $roomTypeQuery");
+        $sth = $dbh->query("SELECT propertyId, location, address, distanceUKC, distanceCCCU, distanceUKM, price, roomType, availableFrom, furnishing, timestamp
+                            FROM properties
+                            WHERE location='$user->lookingIn'
+                            AND price BETWEEN $user->minPrice
+                            AND $user->maxPrice
+                            AND availableFrom >= STR_TO_DATE('$user->availableFrom', '%Y-%m-%d') $roomTypeQuery
+                            AND propertyId NOT IN (SELECT propertyId FROM shortlist WHERE properties.propertyId=shortlist.propertyId)");
         $sth->setFetchMode(PDO::FETCH_OBJ);
         $results = $sth->fetchAll();
 
