d2830f24b65ed2d04232a08b2a3ea704e7e9597c
jamesl1001 <jamesl1001@msn.com>
2014-07-29 20:45:31 +0100

    Add image captions to gallery

--- views/gallery.php
+++ views/gallery.php
@@ -2,4 +2,5 @@
 
 <?php foreach($images as $image): ?>
     <img src="/img/properties/<?= $propertyId; ?>/<?= $image; ?>"/>
+    <div class="image-caption"><?= str_replace('.jpg', '', $image); ?></div>
 <?php endforeach; ?>
