c28c5cf67570d9a6907efb6df94361f02d7136d5
jamesl1001 <jamesl1001@msn.com>
2014-08-09 22:14:55 +0100

    Admin styles

--- views/admin.php
+++ views/admin.php
@@ -1,37 +1,43 @@
 <main>
-    <h1>Admin Tools</h1>
+    <div class="padding">
+        <h1 class="h1--show">Admin Tools</h1>
 
-    <?php if(!isset($_SESSION['s_admin'])): ?>
-        <form action="" method="post">
-            <label for="login-password">Password</label>
-            <input type="password" name="login-password" autofocus required/>
+        <?php if(!isset($_SESSION['s_admin'])): ?>
+            <form action="" method="post">
+                <div class="form-row">
+                    <label for="login-password">Password</label>
+                    <input type="password" name="login-password" autofocus required/>
+                </div>
 
-            <input type="submit" name="login-submit" value="Login"/>
-        </form>
-    <?php endif; ?>
+                <div class="form-row">
+                    <input type="submit" name="login-submit" value="Login"/>
+                </div>
+            </form>
+        <?php endif; ?>
 
-    <?php
-    require_once('php/hash.php');
+        <?php
+        require_once('php/hash.php');
 
-    if(!empty($_POST['login-submit'])) {
-        if(!empty($_POST['login-password'])) {
-            if($hash === hash('sha256', $_POST['login-password'])) {
-                $_SESSION['s_admin'] = true;
-                header('location: admin');
+        if(!empty($_POST['login-submit'])) {
+            if(!empty($_POST['login-password'])) {
+                if($hash === hash('sha256', $_POST['login-password'])) {
+                    $_SESSION['s_admin'] = true;
+                    header('location: admin');
+                } else {
+                    echo '<p class="error">Wrong password.</p>';
+                }
             } else {
-                return '<p class="error">Wrong password.</p>';
+                echo '<p class="error">Please enter a password.</p>';
             }
-        } else {
-            echo '<p class="error">Please enter a password.</p>';
         }
-    }
-    ?>
+        ?>
 
-    <?php if(isset($_SESSION['s_admin'])): ?>
-        <p>Logged in as admin.</p>
+        <?php if(isset($_SESSION['s_admin'])): ?>
+            <p>Logged in as admin.</p>
 
-        <p><a href="/admin/activity">View user activity</a> - View the activity of every user</p>
-        <p><a href="/admin/new-property">Add a new property</a> - Use this form to add new properties to the application</p>
-        <p><a href="/admin/update-room-availability">Update a room's availability</a> - If a room has become occupied, it's availability can be changed here</p>
-    <?php endif; ?>
+            <p><a href="/admin/activity">View user activity</a> - View the activity of every user</p>
+            <p><a href="/admin/new-property">Add a new property</a> - Use this form to add new properties to the application</p>
+            <p><a href="/admin/update-room-availability">Update a room's availability</a> - If a room has become occupied, it's availability can be changed here</p>
+        <?php endif; ?>
+    </div>
 </main>
--- views/admin/update-room-availability.php
+++ views/admin/update-room-availability.php
@@ -1,25 +1,27 @@
 <main>
-    <h1>Admin Update Room Availability</h1>
+    <div class="padding">
+        <h1 class="h1--show">Admin Update Room Availability</h1>
 
-    <table>
-        <thead>
-            <tr>
-                <th>Address</th>
-                <th>Room number</th>
-                <th>Availability</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach($properties as $i => $property): ?>
+        <table>
+            <thead>
                 <tr>
-                    <td><?= $property->address; ?></td>
-                    <td><?= $property->roomNo; ?></td>
-                    <td>
-                        <input type="checkbox" id="checkbox-<?= $i; ?>" class="availability" data-room-id="<?= $property->roomId; ?>"<?= ($property->availability == 1 ? ' checked' : ''); ?>/>
-                        <label for="checkbox-<?= $i; ?>" class="checkbox-style"></label>
-                    </td>
+                    <th>Address</th>
+                    <th>Room number</th>
+                    <th>Availability</th>
                 </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php foreach($properties as $i => $property): ?>
+                    <tr>
+                        <td><?= $property->address; ?></td>
+                        <td><?= $property->roomNo; ?></td>
+                        <td>
+                            <input type="checkbox" id="checkbox-<?= $i; ?>" class="availability" data-room-id="<?= $property->roomId; ?>"<?= ($property->availability == 1 ? ' checked' : ''); ?>/>
+                            <label for="checkbox-<?= $i; ?>" class="checkbox-style"></label>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
 </main>
