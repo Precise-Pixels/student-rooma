69bd5c1599882f2964e2f6601264b557dbe62060
jamesl1001 <jamesl1001@msn.com>
2014-08-10 23:15:12 +0100

    Property info styles

--- views/property.php
+++ views/property.php
@@ -14,9 +14,9 @@
                     <?php foreach($property->rooms as $room): ?>
                         <div class="room room--<?= ($room->availability ? 'available' : 'unavailable'); ?>">
                             <img src="/img/properties/<?= $property->propertyId; ?>/Room <?= $room->roomNo; ?>.jpg"/>
-                            <div class="padding">
-                                <span class="room-price">£<?= $room->price; ?>pcm</span>
-                                <span class="room-type"><?= $room->roomType; ?> room</span>
+                            <div class="room-info padding">
+                                <span class="room-price">£<?= $room->price; ?><span>pcm</span></span>
+                                <span class="room-type"><i class="ico-bed"></i><?= ucfirst($room->roomType); ?> room</span>
                             </div>
                         </div>
                     <?php endforeach; ?>
@@ -31,20 +31,27 @@
         </div>
 
         <div class="padding">
-            <p><?= $property->address; ?></p>
+            <hr>
+            <p class="property-address"><i class="ico-pointer"></i><?= $property->address; ?></p>
 
-            <?php if($property->location == 'Canterbury'): ?>
-                <p><?= $property->distanceUKC; ?>m walk to UKC</p>
-                <p><?= $property->distanceCCCU; ?>m walk to CCCU</p>
-            <?php else: ?>
-                <p><?= $property->distanceUKM; ?>m walk to UKM</p>
-            <?php endif; ?>
+            <div class="property-col property-col--l">
+                <p><span>Date available:</span> <?= $property->availableFrom; ?></p>
+                <p><span>First listed:</span> <?= $property->timestamp; ?></p>
+            </div>
 
-            <p>Date available: <?= $property->availableFrom; ?></p>
+            <div class="property-col property-col--r">
+                <?php if($property->location == 'Canterbury'): ?>
+                    <p><span>Walk to UKC:</span> <?= $property->distanceUKC; ?>m</p>
+                    <p><span>Walk to CCCU:</span> <?= $property->distanceCCCU; ?>m</p>
+                <?php else: ?>
+                    <p><span>Walk to UKM:</span> <?= $property->distanceUKM; ?>m</p>
+                <?php endif; ?>
+            </div>
 
-            <p>Info: <?= nl2br(stripcslashes($property->info)); ?></p>
-
-            <p>First listed: <?= $property->timestamp; ?></p>
+            <p class="property-info">
+                <span>General information:</span>
+                <?= nl2br(stripcslashes($property->info)); ?>
+            </p>
 
             <a href="http://maps.google.com/maps?q=<?= $property->address; ?>" target="_blank"><img src="http://maps.googleapis.com/maps/api/staticmap?center=<?= $property->address; ?>&zoom=15&size=200x100&maptype=roadmap&key=AIzaSyCNlx7Q6EFJ2nlJfkAnMIsCm94fdSzaqf4" class="property-map"/></a>
         </div>
