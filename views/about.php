e47ef47f58a56171030319567e473dff2dc6841d
jamesl1001 <jamesl1001@msn.com>
2014-08-10 12:44:29 +0100

    About copy

--- views/about.php
+++ views/about.php
@@ -1,12 +1,24 @@
 <main>
     <div class="padding">
-        <h1>About this app</h1>
+        <h1 class="h1--show">About this app</h1>
 
         <h2>What is Student Rooma?</h2>
         <p>...</p>
 
-        <h2>Browser Support</h2>
-        <p>...</p>
+        <h2>Browser Requirements</h2>
+        <p>This app has been tested and is fully functional on the following browsers:</p>
+
+        <ul>
+            <li>Chrome ?</li>
+            <li>Firefox ?</li>
+            <li>Safari ? (Windows)</li>
+            <li>Opera ?</li>
+            <li>IE9+</li>
+            <li>Android Browser ?</li>
+            <li>...</li>
+        </ul>
+
+        <p>This app also requires Javascript and cookies to be enabled at all times.</p>
 
         <hr>
 
