df2ea60f845613ce86604e7d2b17a2ba5b380066
jamesl1001 <jamesl1001@msn.com>
2014-08-11 18:50:27 +0100

    Request an appropriately sized map depending on the screen width. Request jpg map rather than png.

--- js/image-loader.js
+++ js/image-loader.js
@@ -3,9 +3,11 @@ loadImages();
 function loadImages() {
     var property = document.getElementsByClassName('property--active');
     var map      = property[0].getElementsByClassName('property-map');
+    var width    = Math.round((window.innerWidth || document.documentElement.clientWidth));
+    var height   = Math.round(width * 0.5625);
 
     var img = new Image();
-    img.src = map[0].getAttribute('data-src');
+    img.src = map[0].getAttribute('data-src').replace('WIDTH', width).replace('HEIGHT', height);
     img.className = 'property-map';
 
     img.addEventListener('load', function() {
