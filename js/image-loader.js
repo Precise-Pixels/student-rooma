cdc281228b879dd0691189b810ca0ae19ded6549
jamesl1001 <jamesl1001@msn.com>
2014-08-12 16:57:26 +0100

    Don't continue with loadImages() and loadSlider() if no properties

--- js/image-loader.js
+++ js/image-loader.js
@@ -1,42 +1,45 @@
 loadImages();
 
 function loadImages() {
-    var property     = document.getElementsByClassName('property--active');
-    var roomsWrapper = property[0].getElementsByClassName('rooms');
-    var rooms        = property[0].getElementsByClassName('room-image');
-    var map          = property[0].getElementsByClassName('property-map');
-    var width        = Math.round((window.innerWidth || document.documentElement.clientWidth));
-    var height       = Math.round(width * 0.5625);
-    var imagesLoaded = 0;
+    var property = document.getElementsByClassName('property--active');
 
-    // Rooms
-    for(var i = 0, l = rooms.length; i < l; i++) {
+    if(property.length) {
+        var roomsWrapper = property[0].getElementsByClassName('rooms');
+        var rooms        = property[0].getElementsByClassName('room-image');
+        var map          = property[0].getElementsByClassName('property-map');
+        var width        = Math.round((window.innerWidth || document.documentElement.clientWidth));
+        var height       = Math.round(width * 0.5625);
+        var imagesLoaded = 0;
+
+        // Rooms
+        for(var i = 0, l = rooms.length; i < l; i++) {
+            var img = new Image();
+            img.src = rooms[i].getAttribute('data-src');
+            img.className = 'room-image';
+
+            img.addEventListener('load', function() {
+                imagesLoaded++;
+                if(imagesLoaded === rooms.length) {
+                    roomsWrapper[0].className += ' rooms--loaded';
+                }
+            });
+
+            rooms[i].parentNode.replaceChild(img, rooms[i]);
+        }
+
+        // Map
+        if(width > 640 || height > 640) {
+            width = 640;
+            height = 360;
+        }
+
         var img = new Image();
-        img.src = rooms[i].getAttribute('data-src');
-        img.className = 'room-image';
+        img.src = map[0].childNodes[0].getAttribute('data-src').replace('WIDTH', width).replace('HEIGHT', height);
 
         img.addEventListener('load', function() {
-            imagesLoaded++;
-            if(imagesLoaded === rooms.length) {
-                roomsWrapper[0].className += ' rooms--loaded';
-            }
+            map[0].className = 'property-map property-map--loaded';
         });
 
-        rooms[i].parentNode.replaceChild(img, rooms[i]);
+        map[0].replaceChild(img, map[0].childNodes[0]);
     }
-
-    // Map
-    if(width > 640 || height > 640) {
-        width = 640;
-        height = 360;
-    }
-
-    var img = new Image();
-    img.src = map[0].childNodes[0].getAttribute('data-src').replace('WIDTH', width).replace('HEIGHT', height);
-
-    img.addEventListener('load', function() {
-        map[0].className = 'property-map property-map--loaded';
-    });
-
-    map[0].replaceChild(img, map[0].childNodes[0]);
 }
--- js/image-slider.js
+++ js/image-slider.js
@@ -1,94 +1,97 @@
 loadSlider();
 
 function loadSlider() {
-    var property     = document.getElementsByClassName('property--active');
-    var slider       = property[0].getElementsByClassName('room-slider');
-    var totalSlides  = property[0].getElementsByClassName('room').length;
-    var controls     = property[0].getElementsByClassName('room-controls');
-    var slideWidth   = controls[0].offsetWidth;
-    var left         = controls[0].getElementsByClassName('room-control--left');
-    var right        = controls[0].getElementsByClassName('room-control--right');
-    var tabs         = document.getElementsByClassName('tab');
-    var currentSlide = 0;
+    var property = document.getElementsByClassName('property--active');
 
-    controls[0].addEventListener('touchstart', sliderStart);
-    controls[0].addEventListener('touchmove', sliderMove);
-    controls[0].addEventListener('touchend', sliderEnd);
-    left[0].addEventListener('click', sliderLeft);
-    right[0].addEventListener('click', sliderRight);
+    if(property.length) {
+        var slider       = property[0].getElementsByClassName('room-slider');
+        var totalSlides  = property[0].getElementsByClassName('room').length;
+        var controls     = property[0].getElementsByClassName('room-controls');
+        var slideWidth   = controls[0].offsetWidth;
+        var left         = controls[0].getElementsByClassName('room-control--left');
+        var right        = controls[0].getElementsByClassName('room-control--right');
+        var tabs         = document.getElementsByClassName('tab');
+        var currentSlide = 0;
 
-    for(var i = 0, l = tabs.length; i < l; i++) {
-        tabs[i].addEventListener('click', clickTabs);
-    }
+        controls[0].addEventListener('touchstart', sliderStart);
+        controls[0].addEventListener('touchmove', sliderMove);
+        controls[0].addEventListener('touchend', sliderEnd);
+        left[0].addEventListener('click', sliderLeft);
+        right[0].addEventListener('click', sliderRight);
 
-    var touchStart,
-        touchMove,
-        move;
-
-    slider[0].style.width = totalSlides * 100 + '%';
-    slider[0].className = 'room-slider room-slider--loaded';
-
-    updateTabs();
-
-    function sliderStart(e) {
-        slider[0].className = 'room-slider room-slider--loaded';
-        touchStart = e.touches[0].pageX;
-    }
-
-    function sliderMove(e) {
-        touchMove = e.touches[0].pageX;
-
-        // Don't overscroll if on the last slide
-        if(currentSlide !== totalSlides - 1 || touchStart < touchMove) {
-            move = currentSlide * slideWidth + touchStart - touchMove;
-            slider[0].style.transform = 'translate3d(-' + move + 'px,0,0)';
-        }
-    }
-
-    function sliderEnd(e) {
-        if((touchStart - touchMove) > slideWidth / 4 && currentSlide < totalSlides - 1) {
-            currentSlide++;
-        }
-
-        if((touchStart - touchMove) < ((slideWidth / 4) * -1) && currentSlide > 0) {
-            currentSlide--;
-        }
-
-        animateSlide();
-        updateTabs();
-    }
-
-    function sliderLeft() {
-        if(currentSlide > 0) {
-            currentSlide--;
-            animateSlide();
-            updateTabs();
-        }
-    }
-
-    function sliderRight() {
-        if(currentSlide < totalSlides - 1) {
-            currentSlide++;
-            animateSlide();
-            updateTabs();
-        }
-    }
-
-    function clickTabs(e) {
-        currentSlide = +e.target.className.match(/\d+/)[0];
-        animateSlide();
-        updateTabs();
-    }
-
-    function updateTabs() {
         for(var i = 0, l = tabs.length; i < l; i++) {
-            tabs[i].className = tabs[i].className.replace(' tab--active', '');
+            tabs[i].addEventListener('click', clickTabs);
         }
-        tabs[currentSlide].className += ' tab--active';
-    }
 
-    function animateSlide() {
-        slider[0].className = 'room-slider room-slider--loaded room-slider--animate';
-        slider[0].style.transform = 'translate3d(-' + currentSlide * slideWidth + 'px,0,0)';
+        var touchStart,
+            touchMove,
+            move;
+
+        slider[0].style.width = totalSlides * 100 + '%';
+        slider[0].className = 'room-slider room-slider--loaded';
+
+        updateTabs();
+
+        function sliderStart(e) {
+            slider[0].className = 'room-slider room-slider--loaded';
+            touchStart = e.touches[0].pageX;
+        }
+
+        function sliderMove(e) {
+            touchMove = e.touches[0].pageX;
+
+            // Don't overscroll if on the last slide
+            if(currentSlide !== totalSlides - 1 || touchStart < touchMove) {
+                move = currentSlide * slideWidth + touchStart - touchMove;
+                slider[0].style.transform = 'translate3d(-' + move + 'px,0,0)';
+            }
+        }
+
+        function sliderEnd(e) {
+            if((touchStart - touchMove) > slideWidth / 4 && currentSlide < totalSlides - 1) {
+                currentSlide++;
+            }
+
+            if((touchStart - touchMove) < ((slideWidth / 4) * -1) && currentSlide > 0) {
+                currentSlide--;
+            }
+
+            animateSlide();
+            updateTabs();
+        }
+
+        function sliderLeft() {
+            if(currentSlide > 0) {
+                currentSlide--;
+                animateSlide();
+                updateTabs();
+            }
+        }
+
+        function sliderRight() {
+            if(currentSlide < totalSlides - 1) {
+                currentSlide++;
+                animateSlide();
+                updateTabs();
+            }
+        }
+
+        function clickTabs(e) {
+            currentSlide = +e.target.className.match(/\d+/)[0];
+            animateSlide();
+            updateTabs();
+        }
+
+        function updateTabs() {
+            for(var i = 0, l = tabs.length; i < l; i++) {
+                tabs[i].className = tabs[i].className.replace(' tab--active', '');
+            }
+            tabs[currentSlide].className += ' tab--active';
+        }
+
+        function animateSlide() {
+            slider[0].className = 'room-slider room-slider--loaded room-slider--animate';
+            slider[0].style.transform = 'translate3d(-' + currentSlide * slideWidth + 'px,0,0)';
+        }
     }
 }
