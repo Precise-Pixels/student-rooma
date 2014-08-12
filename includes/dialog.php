7b49da50637cf20e15e024e11189c2ae9ba46d16
jamesl1001 <jamesl1001@msn.com>
2014-08-12 16:38:11 +0100

    Swap round Y and N buttons on dialogs

--- includes/dialog.php
+++ includes/dialog.php
@@ -2,7 +2,7 @@
     <h1 id="dialog-heading"></h1>
     <div id="dialog-text-wrapper" class="padding"></div>
     <div id="dialog-buttons" class="padding">
-        <button id="dialog-button-y"></button>
         <button id="dialog-button-n"></button>
+        <button id="dialog-button-y"></button>
     </div>
 </div>
