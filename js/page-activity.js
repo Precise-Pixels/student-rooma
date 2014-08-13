c35edeed2f03b7c4db39ec923def7546469390c8
jamesl1001 <jamesl1001@msn.com>
2014-08-13 15:41:05 +0100

    URL hash is changed when using Activity tabs

--- js/page-activity.js
+++ js/page-activity.js
@@ -9,19 +9,30 @@ tabBooked.addEventListener('click', function() {
     resetTabs();
     tabBooked.className = 'tab tab--active';
     booked.className    = 'tab-content tab-content--show';
+    changeHash('booked');
 });
 
 tabStarred.addEventListener('click', function() {
     resetTabs();
     tabStarred.className = 'tab tab--active';
     starred.className    = 'tab-content tab-content--show';
+    changeHash('starred');
 });
 
 tabNos.addEventListener('click', function() {
     resetTabs();
     tabNos.className = 'tab tab--active';
     nos.className    = 'tab-content tab-content--show';
+    changeHash('nos');
 });
+
+function changeHash(hash) {
+    if('replaceState' in history) {
+        history.replaceState({}, '', '#' + hash);
+    } else {
+        location.hash = hash;
+    }
+}
 
 function resetTabs() {
     tabBooked.className  = 'tab';
