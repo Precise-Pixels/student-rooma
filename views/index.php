7dc9bfee839220dfcf8fd468bbf7dbc1343b55fb
jamesl1001 <jamesl1001@msn.com>
2014-08-14 23:03:47 +0100

    Add cookie message to index

--- views/index.php
+++ views/index.php
@@ -10,7 +10,10 @@
             <div data-src="http://www.youtube.com/embed/GUNMBw6p73k?showinfo=0&amp;controls=0&amp;rel=0" id="youtube"></div>
         </div>
 
-        <p id="small-print">If you click "Continue with Facebook" and are not already registered with Student Rooma, you will be registered and you agree to Student Rooma's <a href="/terms-and-conditions">Terms &amp; Conditions</a> and <a href="/privacy-policy">Privacy Policy</a>.</p>
+        <div id="small-print">
+            <p>If you click "Continue with Facebook" and are not already registered with Student Rooma, you will be registered and you agree to Student Rooma's <a href="/terms-and-conditions">Terms &amp; Conditions</a> and <a href="/privacy-policy">Privacy Policy</a>.</p>
+            <p>This site uses cookies. Some of the cookies we use are essential for the normal operation of this site. Please ensure cookies remain enabled during your use of this site. For further information, please visit our <a href="/privacy-policy">Privacy Policy</a>.</p>
+        </div>
 
         <p id="copyright">&copy; 2014, Student Rooma, All Rights Reserved</p>
 
