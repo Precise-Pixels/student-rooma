3298ab52338a1010743c90f3cc465435bf7220b5
jamesl1001 <jamesl1001@msn.com>
2014-08-17 12:47:31 +0100

    Remove non-admin if-statements from header and scripts

--- includes/header.php
+++ includes/header.php
@@ -16,39 +16,6 @@
             <img src="/img/logo.png" alt="Student Rooma" id="logo"/>
         <?php endif; ?>
 
-        <?php if($isProfile): ?>
-            <div id="header-btn-l"><i class="ico-cross ico--centre"><span>Cancel</span></i></div>
-            <div id="header-btn-r"><i class="ico-tick ico--centre"><span>Save</span></i></div>
-        <?php endif; ?>
-
-        <?php if($isProperties): ?>
-            <a href="/activity#starred"><div id="header-btn-l"><i class="ico-activity ico--centre"><span>Activity</span></i></div></a>
-            <a href="/profile"><div id="header-btn-r"><i class="ico-profile ico--centre"><span>Profile</span></i></div></a>
-        <?php endif; ?>
-
-        <?php if($isActivity): ?>
-            <a href="/properties"><div id="header-btn-l"><i class="ico-arrow-back ico--centre"><span>Back to Properties</span></i></div></a>
-            <a href="/profile"><div id="header-btn-r"><i class="ico-profile ico--centre"><span>Profile</span></i></div></a>
-        <?php endif; ?>
-
-        <?php if($isProperty): ?>
-            <a href="/activity#starred"><div id="header-btn-l"><i class="ico-arrow-back ico--centre"><span>Back to Activity</span></i></div></a>
-            <a href="/profile"><div id="header-btn-r"><i class="ico-profile ico--centre"><span>Profile</span></i></div></a>
-        <?php endif; ?>
-
-        <?php if($isGallery): ?>
-            <a href="<?= $_SERVER['HTTP_REFERER']; ?>"><div id="header-btn-l"><i class="ico-arrow-back ico--centre"><span>Back</span></i></div></a>
-            <a href="/profile"><div id="header-btn-r"><i class="ico-profile ico--centre"><span>Profile</span></i></div></a>
-        <?php endif; ?>
-
-        <?php if($isAbout): ?>
-            <a href="/profile"><div id="header-btn-l"><i class="ico-arrow-back ico--centre"><span>Back</span></i></div></a>
-        <?php endif; ?>
-
-        <?php if($isTerms || $isPrivacy): ?>
-            <a href="/"><div id="header-btn-l"><i class="ico-arrow-back ico--centre"><span>Back</span></i></div></a>
-        <?php endif; ?>
-
         <?php if($isAdminActivity || $isAdminAllProperties || $isAdminNewProperty || $isAdminUpdateRoomAvailability): ?>
             <a href="/admin"><div id="header-btn-l"><i class="ico-arrow-back ico--centre"><span>Back</span></i></div></a>
         <?php endif; ?>
--- includes/scripts.php
+++ includes/scripts.php
@@ -3,38 +3,6 @@
 
     <script src="/js/dialog.js"></script>
 
-    <?php if($isIndex): ?>
-        <script src="/js/page-index.js"></script>
-    <?php endif; ?>
-
-    <?php if($isProfile): ?>
-        <script src="/js/page-profile.js"></script>
-    <?php endif; ?>
-
-    <?php if($isProperties): ?>
-        <script src="/js/decision.js"></script>
-        <script src="/js/image-slider.js"></script>
-        <script src="/js/image-loader.js"></script>
-    <?php endif; ?>
-
-    <?php if($isActivity): ?>
-        <script src="/js/decision.js"></script>
-        <script src="/js/page-activity.js"></script>
-    <?php endif; ?>
-
-    <?php if($isProperty): ?>
-        <script src="/js/image-slider.js"></script>
-        <script src="/js/image-loader.js"></script>
-    <?php endif; ?>
-
-    <?php if($isGallery): ?>
-        <script src="/js/decision.js"></script>
-    <?php endif; ?>
-
-    <?php if($isLogout): ?>
-        <script src="/js/page-logout.js"></script>
-    <?php endif; ?>
-
     <?php if($isAdminNewProperty): ?>
         <script src="/js/page-admin-new-property.js"></script>
     <?php endif; ?>
@@ -47,38 +15,6 @@
 // LIVE ?>
 
     <script src="/build/dialog.min.js"></script>
-
-    <?php if($isIndex): ?>
-        <script src="/build/page-index.min.js"></script>
-    <?php endif; ?>
-
-    <?php if($isProfile): ?>
-        <script src="/build/page-profile.min.js"></script>
-    <?php endif; ?>
-
-    <?php if($isProperties): ?>
-        <script src="/build/decision.min.js"></script>
-        <script src="/build/image-slider.min.js"></script>
-        <script src="/build/image-loader.min.js"></script>
-    <?php endif; ?>
-
-    <?php if($isActivity): ?>
-        <script src="/build/decision.min.js"></script>
-        <script src="/build/page-activity.min.js"></script>
-    <?php endif; ?>
-
-    <?php if($isProperty): ?>
-        <script src="/build/image-slider.min.js"></script>
-        <script src="/build/image-loader.min.js"></script>
-    <?php endif; ?>
-
-    <?php if($isGallery): ?>
-        <script src="/build/decision.min.js"></script>
-    <?php endif; ?>
-
-    <?php if($isLogout): ?>
-        <script src="/build/page-logout.min.js"></script>
-    <?php endif; ?>
 
     <?php if($isAdminNewProperty): ?>
         <script src="/build/page-admin-new-property.min.js"></script>
