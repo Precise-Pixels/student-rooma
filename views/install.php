2926ae95647ce2ca396de65ab15bbe8e7ed6617d
jamesl1001 <jamesl1001@msn.com>
2014-08-19 20:52:01 +0100

    Add Install This App instructions

--- views/install.php
+++ views/install.php
@@ -1,6 +1,69 @@
 <main>
     <div class="padding">
         <h1 class="h1--show">Install This App On Your Device</h1>
-        <p>...</p>
+
+        <p>Add Student Rooma to your homescreen to have quick and easy access to it at any time.</p>
+        <p>Follow the instructions for your browser below.</p>
+
+        <h2>Chrome</h2>
+        <p>
+            <ol>
+                <li>Go <a href="/">back to the homepage</a></li>
+                <li>Tap the 'More/Menu' icon</li>
+                <li>Tap 'Add to home screen'</li>
+            </ol>
+        </p>
+
+        <h2>Safari iOS 7</h2>
+        <p>
+            <ol>
+                <li>Tap the 'Share' icon</li>
+                <li>Tap 'Add to Home Screen'</li>
+                <li>Change the URL to: http://studentrooma.co.uk</li>
+                <li>Tap 'Add'</li>
+            </ol>
+        </p>
+
+        <h2>Safari iOS 6-</h2>
+        <p>
+            <ol>
+                <li>Go <a href="/">back to the homepage</a></li>
+                <li>Tap the 'Share' icon</li>
+                <li>Tap 'Add to Home Screen'</li>
+                <li>Tap 'Add'</li>
+            </ol>
+        </p>
+
+        <h2>Android Browser 4.x</h2>
+        <p>
+            <ol>
+                <li>Tap the 'More/Menu' icon</li>
+                <li>Tap 'Save to bookmarks'</li>
+                <li>Change the URL to: http://studentrooma.co.uk</li>
+                <li>Change 'Add to' to 'Home screen'</li>
+                <li>Tap 'OK'</li>
+            </ol>
+        </p>
+
+        <h2>Android Browser 3.x-</h2>
+        <p>
+            <ol>
+                <li>Tap the 'Bookmark' icon or tap the 'Menu' button then 'Bookmarks'</li>
+                <li>Tap 'Add'</li>
+                <li>Change the URL to: http://studentrooma.co.uk</li>
+                <li>Tap 'OK'</li>
+                <li>Tap and hold the newly created bookmark</li>
+                <li>Tap 'Add shortcut to Home'</li>
+            </ol>
+        </p>
+
+        <h2>Windows Phone</h2>
+        <p>
+            <ol>
+                <li>Go <a href="/">back to the homepage</a></li>
+                <li>Tap the 'More' icon</li>
+                <li>Tap 'Pin to Start'</li>
+            </ol>
+        </p>
     </div>
 </main>
