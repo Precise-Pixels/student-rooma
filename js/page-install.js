00d5aca77c4f8e8d8839cfd276b739a036c352d2
jamesl1001 <jamesl1001@msn.com>
2014-08-19 21:14:47 +0100

    Stop after browser matches

--- js/page-install.js
+++ js/page-install.js
@@ -1,23 +1,17 @@
 if(bowser.chrome) {
     location.hash = 'chrome';
-}
-
-if(bowser.ios) {
+} else if(bowser.ios) {
     if(bowser.version == 7) {
         location.hash = 'ios-7';
     } else {
         location.hash = 'ios-6';
     }
-}
-
-if(bowser.android) {
+} else if(bowser.android) {
     if(bowser.version == 4) {
         location.hash = 'android-4';
     } else {
         location.hash = 'android-3';
     }
-}
-
-if(bowser.windowsphone) {
+} else if(bowser.windowsphone) {
     location.hash = 'windows-phone';
 }
