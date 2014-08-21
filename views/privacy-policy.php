f0f7f1a541ec18c2ca8d88bf9b6a0042e8cc7ad8
jamesl1001 <jamesl1001@msn.com>
2014-08-21 16:34:44 +0100

    Add Privacy Policy copy

--- views/privacy-policy.php
+++ views/privacy-policy.php
@@ -1,6 +1,50 @@
 <main>
     <div class="padding">
         <h1 class="h1--show">Privacy Policy</h1>
-        <p>...</p>
+
+        <h2>Information We Collect</h2>
+        <p>In running and maintaining our application we may collect and process the following data about you:</p>
+        <ul>
+            <li>Information about your use of our application including details of your visits such as pages viewed and the resources that you access. Such information includes but is not exclusive to traffic data, location data and other communication data.</li>
+            <li>Information provided voluntarily by you. For example, when you sign into the application or enter your details on the profile page.</li>
+            <li>Information that you provide when you communicate with us by any means.</li>
+        </ul>
+
+        <h2>Use of Cookies</h2>
+        <p>When using our application, we may set cookies on your device in order to assist with providing you a great user experience.</p>
+        <p>We or our third-party providers may set the following cookies whilst using our application:</p>
+        <ul>
+            <li>Session cookies - set by us to ensure that you remain signed in throughout the application</li>
+            <li>Facebook - set by Faceook to log you in to the application</li>
+            <li>Google Analytics - set by Google to provide us with useful statistics regarding your visit</li>
+        </ul>
+        <p>You can adjust the settings on your device to decline any cookies if you wish. This can easily be done by activating the reject cookies setting on your device. However in doing so, the application may not function correctly.</p>
+
+        <h2>Use Of Your Information</h2>
+        <p>We use the information that we collect from you to provide our services to you.</p>
+        <p>None of the information you provide is publically available.</p>
+        <p>Your phone number is used to contact you once you have booked properties to view.</p>
+        <p>When you login in using Facebook, we collect the following information from your account:</p>
+        <ul>
+            <li>Facebook ID</li>
+            <li>Full name</li>
+            <li>Current location</li>
+        </ul>
+
+        <h2>Storing Your Personal Data</h2>
+        <p>In operating our application it is necessary to transfer data that we collect from you to locations outside of the European Union for processing and storing. By providing your personal data to us, you agree to this transfer, storing or processing. We do our utmost to ensure that all reasonable steps are taken to make sure that your data is stored securely.</p>
+        <p>Unfortunately the sending of information via the internet is not totally secure and on occasion such information can be intercepted. We cannot guarantee the security of data that you choose to send us electronically. Sending such information is entirely at your own risk.</p>
+
+        <h2>Disclosing Your Personal Information</h2>
+        <p>We will not disclose your personal information to any other party without your prior permission other than in accordance with this Privacy Policy and where we are legally required by law.</p>
+
+        <h2>Third Party Links</h2>
+        <p>On occasion we include links to third parties on this application. Where we provide a link it does not mean that we endorse or approve that site's policy towards visitor privacy. You should review their privacy policy before sending them any personal data.</p>
+
+        <h2>Access To Information</h2>
+        <p>In accordance with the Data Protection Act 1998 you have the right to access any information that we hold relating to you. Please note that we reserve the right to charge a fee of £10 to cover costs incurred by us in providing you with the information.</p>
+
+        <h2>Contact Us</h2>
+        <p>Please do not hesitate to contact us regarding any matter relating to this Privacy Policy at <a href="&#109;ailto:support&#64;studentrooma&#46;co&#46;uk">support&#64;studentrooma&#46;co&#46;uk</a></p>
     </div>
 </main>
