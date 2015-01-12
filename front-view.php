cc49ef2048649be36628bbcbcd911186caabb1f8
jamesl1001 <jamesl1001@msn.com>
2015-01-12 15:50:45 +0000

    Add logic to differentiate between landing and app header

--- front-view.php
+++ front-view.php
@@ -7,7 +7,7 @@
 Precise Pixels | http://precisepixels.co.uk
 https://github.com/Precise-Pixels/student-rooma
 -->
-<body<?= ($isAppIndex ? ' class="index"' : ''); ?>>
+<body<?= ($isAppIndex ? ' class="app-index"' : ''); ?><?= ($isLanding ? ' class="landing"' : ''); ?>>
     <?php require_once('includes/header.php'); ?>
     <?php require_once("views/$file.php"); ?>
     <?php require_once('includes/dialog.php'); ?>
--- includes/header.php
+++ includes/header.php
@@ -1,55 +1,63 @@
-<?php if($isAppIndex): ?>
+<?php if($isApp): ?>
+    <?php if($isAppIndex): ?>
 
-    <header>
-        <img src="/img/logo.png" alt="Student Rooma" id="logo"/>
-        <p id="slogan">Find your perfect room in Canterbury &amp; Medway</p>
-    </header>
-
-<?php else: ?>
-
-    <header>
-        <a href="/app/">
+        <header>
             <img src="/img/logo.png" alt="Student Rooma" id="logo"/>
-        </a>
+            <p id="slogan">Find your perfect room in Canterbury &amp; Medway</p>
+        </header>
 
-        <?php if($isProfile): ?>
-            <?php if(!isset($_SESSION['s_firstTime']) || !$_SESSION['s_firstTime']): ?>
-                <div id="header-btn-l" tabindex="1"><i class="ico-cross ico--centre"><span>Cancel</span></i></div>
+    <?php else: ?>
+
+        <header>
+            <a href="/app/">
+                <img src="/img/logo.png" alt="Student Rooma" id="logo"/>
+            </a>
+
+            <?php if($isProfile): ?>
+                <?php if(!isset($_SESSION['s_firstTime']) || !$_SESSION['s_firstTime']): ?>
+                    <div id="header-btn-l" tabindex="1"><i class="ico-cross ico--centre"><span>Cancel</span></i></div>
+                <?php endif; ?>
+                <div id="header-btn-r" tabindex="2"><i class="ico-tick ico--centre"><span>Save</span></i></div>
             <?php endif; ?>
-            <div id="header-btn-r" tabindex="2"><i class="ico-tick ico--centre"><span>Save</span></i></div>
-        <?php endif; ?>
 
-        <?php if($isProperties): ?>
-            <a href="/app/activity#starred" id="header-btn-l"><i class="ico-activity ico--centre"><span>Activity</span></i></a>
-            <a href="/app/profile" id="header-btn-r"><i class="ico-profile ico--centre"><span>Profile</span></i></a>
-        <?php endif; ?>
+            <?php if($isProperties): ?>
+                <a href="/app/activity#starred" id="header-btn-l"><i class="ico-activity ico--centre"><span>Activity</span></i></a>
+                <a href="/app/profile" id="header-btn-r"><i class="ico-profile ico--centre"><span>Profile</span></i></a>
+            <?php endif; ?>
 
-        <?php if($isActivity): ?>
-            <a href="/app/properties" id="header-btn-l"><i class="ico-arrow-back ico--centre"><span>Back to Properties</span></i></a>
-            <a href="/app/profile" id="header-btn-r"><i class="ico-profile ico--centre"><span>Profile</span></i></a>
-        <?php endif; ?>
+            <?php if($isActivity): ?>
+                <a href="/app/properties" id="header-btn-l"><i class="ico-arrow-back ico--centre"><span>Back to Properties</span></i></a>
+                <a href="/app/profile" id="header-btn-r"><i class="ico-profile ico--centre"><span>Profile</span></i></a>
+            <?php endif; ?>
 
-        <?php if($isProperty): ?>
-            <a href="/app/activity#starred" id="header-btn-l"><i class="ico-arrow-back ico--centre"><span>Back to Activity</span></i></a>
-            <a href="/app/profile" id="header-btn-r"><i class="ico-profile ico--centre"><span>Profile</span></i></a>
-        <?php endif; ?>
+            <?php if($isProperty): ?>
+                <a href="/app/activity#starred" id="header-btn-l"><i class="ico-arrow-back ico--centre"><span>Back to Activity</span></i></a>
+                <a href="/app/profile" id="header-btn-r"><i class="ico-profile ico--centre"><span>Profile</span></i></a>
+            <?php endif; ?>
 
-        <?php if($isGallery): ?>
-            <a href="<?= (isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '/app/properties'); ?>" id="header-btn-l"><i class="ico-arrow-back ico--centre"><span>Back</span></i></a>
-            <a href="/app/profile" id="header-btn-r"><i class="ico-profile ico--centre"><span>Profile</span></i></a>
-        <?php endif; ?>
+            <?php if($isGallery): ?>
+                <a href="<?= (isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '/app/properties'); ?>" id="header-btn-l"><i class="ico-arrow-back ico--centre"><span>Back</span></i></a>
+                <a href="/app/profile" id="header-btn-r"><i class="ico-profile ico--centre"><span>Profile</span></i></a>
+            <?php endif; ?>
 
-        <?php if($isInstall || $isAbout): ?>
-            <a href="/app/profile" id="header-btn-l"><i class="ico-arrow-back ico--centre"><span>Back</span></i></a>
-        <?php endif; ?>
+            <?php if($isInstall || $isAbout): ?>
+                <a href="/app/profile" id="header-btn-l"><i class="ico-arrow-back ico--centre"><span>Back</span></i></a>
+            <?php endif; ?>
 
-        <?php if($isPrivacy): ?>
-            <a href="/app/" id="header-btn-l"><i class="ico-arrow-back ico--centre"><span>Back</span></i></a>
-        <?php endif; ?>
+            <?php if($isPrivacy): ?>
+                <a href="/app/" id="header-btn-l"><i class="ico-arrow-back ico--centre"><span>Back</span></i></a>
+            <?php endif; ?>
 
-        <?php if($isAdminActivity || $isAdminAllProperties || $isAdminNewProperty || $isAdminUpdateRoomAvailability || $isAdminDeleteProperty): ?>
-            <a href="/app/admin" id="header-btn-l"><i class="ico-arrow-back ico--centre"><span>Back</span></i></a>
-        <?php endif; ?>
+            <?php if($isAdminActivity || $isAdminAllProperties || $isAdminNewProperty || $isAdminUpdateRoomAvailability || $isAdminDeleteProperty): ?>
+                <a href="/app/admin" id="header-btn-l"><i class="ico-arrow-back ico--centre"><span>Back</span></i></a>
+            <?php endif; ?>
+        </header>
+
+    <?php endif; ?>
+<?php elseif($isLanding): ?>
+
+    <header>
+        HEADER
     </header>
 
 <?php endif; ?>
