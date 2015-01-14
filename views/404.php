658667d09d94c6ca44af5eba3bb68b71e4ed804c
JacobH1992 <ss_clan_cod@hotmail.com>
2015-01-14 17:06:41 +0000

    Added further styles

--- views/404.php
+++ views/404.php
@@ -1,5 +1,5 @@
 <main>
-    <div class="padding">
+    <div class="padding align-center">
         <h1 class="h1--show">404 Page Not Found</h1>
         <p>Sorry, the page you are looking for has not been found.</p>
         <p>Try checking the URL or <a href="/" class="paragraph-a">return to the homepage</a>.</p>
--- views/index.php
+++ views/index.php
@@ -3,18 +3,18 @@
     <div id="section-one" class="padding align-center">
         <h1 class="h1--show">Matching students to their perfect room!</h1>
     	<a href="/app/"><button class="button-large">OPEN APP</button></a>
+    	<img src="/img/landing-page/phone1.png" alt="">
         <div class="landlord-link">
         	<a href="/landlord">Are you a landlord?</a>
         </div>
-    	<img src="/img/landing-page/phone1.png" alt="">
     </div>
 </section>
 
 <section>
     <div id="section-two" class="padding align-center">
-    	<img src="/img/landing-page/phone2.png" alt="">
     	<div class="text-right">
     		<h2>Accomodation at the price, location and style you want.</h2>
+    		<img src="/img/landing-page/phone2.png" alt="">
 			<p>This is student house window shopping from the comfort of the lecture hall, library or SU.</p>
 			<ul>
 				<li>Use filters to find eactly what you want</li>
@@ -24,4 +24,9 @@
 			<p>All at your fingertips.</p>
     	</div>
     </div>
+</section>
+
+<section id="footer">
+	<div class="padding align-center">
+	</div>
 </section>
