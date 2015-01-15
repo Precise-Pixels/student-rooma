b89f495ed7c72351992a47e27df624bcf14e772a
jamesl1001 <jamesl1001@msn.com>
2015-01-15 21:23:46 +0000

    Add placeholder landlords page

---
+++ views/landlords.php
@@ -0,0 +1,10 @@
+<section class="lgrey bg">
+    <div id="section-one" class="section-padding align-center">
+        <h1 class="h1--show">Landlords</h1>
+    </div>
+</section>
+
+<section id="footer">
+    <div class="padding align-center">
+    </div>
+</section>
