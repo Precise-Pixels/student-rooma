a40880712266682a94d2fe0f9c31709e6f890e6b
JacobH1992 <ss_clan_cod@hotmail.com>
2015-01-15 14:00:48 +0000

    Added line decoration

--- views/index.php
+++ views/index.php
@@ -1,34 +1,35 @@
 
-<div id="line1"></div>
-
 <section class="lgrey bg">
     <div id="section-one" class="section-padding align-center">
         <h1 class="h1--show">Matching students to their perfect room!</h1>
-    	<img src="/img/landing-page/phone1.png" alt="">
+        <img src="/img/landing-page/phone1.png" alt="">
         <a href="/app/"><button class="button-large">OPEN APP</button></a>
         <div class="landlord-link">
-        	<a href="/landlord">Are you a landlord?</a>
+            <a href="/landlord">Are you a landlord?</a>
         </div>
+        <div id="line1"></div>
+        <div id="line2"></div>
+        <div id="line3"></div>
     </div>
 </section>
 
 <section>
     <div id="section-two" class="section-padding align-center">
-    	<div class="text-right">
-    		<h2>Accomodation at the price, location and style you want.</h2>
-    		<img src="/img/landing-page/phone2.png" alt="">
-			<p>This is student house window shopping from the comfort of the lecture hall, library or SU.</p>
-			<ul>
-				<li>Use filters to find eactly what you want</li>
-				<li>Dismiss properties or save them for later</li>
-				<li>Arrange viewings easily</li>
-			</ul>
-			<p>All at your fingertips.</p>
-    	</div>
+        <div class="text-right">
+            <h2>Accomodation at the price, location and style you want.</h2>
+            <img src="/img/landing-page/phone2.png" alt="">
+            <p>This is student house window shopping from the comfort of the lecture hall, library or SU.</p>
+            <ul>
+                <li>Use filters to find eactly what you want</li>
+                <li>Dismiss properties or save them for later</li>
+                <li>Arrange viewings easily</li>
+            </ul>
+            <p>All at your fingertips.</p>
+        </div>
     </div>
 </section>
 
 <section id="footer">
-	<div class="padding align-center">
-	</div>
+    <div class="padding align-center">
+    </div>
 </section>
