9a6c4c04132d3099c3a5ba4566fe62baef9d3dd9
jamesl1001 <jamesl1001@msn.com>
2015-01-28 17:15:06 +0000

    Move landing site images into 'landing' directory

--- views/index.php
+++ views/index.php
@@ -1,7 +1,7 @@
 <section class="lgrey bg">
     <div id="section-one" class="section-padding align-center">
         <h1 class="h1--show">Matching students to their perfect room!</h1>
-        <img src="/img/landing-page/phone1.png" alt="">
+        <img src="/img/landing/phone1.png" alt="">
         <a href="/app/"><button class="button-large">OPEN APP</button></a>
         <div class="landlord-link">
             <a href="/landlords">Are you a landlord?</a>
@@ -16,7 +16,7 @@
     <div id="section-two" class="section-padding align-center">
         <div class="text-right">
             <h2>Accomodation at the price, location and style you want.</h2>
-            <img src="/img/landing-page/phone2.png" alt="">
+            <img src="/img/landing/phone2.png" alt="">
             <p>This is student house window shopping from the comfort of the lecture hall, library or SU.</p>
             <ul>
                 <li>Use filters to find eactly what you want</li>
