defa4864e0a227448f08770f7f1c86795e261669
jamesl1001 <jamesl1001@msn.com>
2015-02-05 17:14:43 +0000

    Add referer check to updateRoomAvailability.php

--- php/updateRoomAvailability.php
+++ php/updateRoomAvailability.php
@@ -2,9 +2,11 @@
 
 require('db.php');
 
-$roomId       = $_POST['roomId'];
-$availability = $_POST['availability'];
+if(preg_match('/admin\/update-room-availability/', $_SERVER['HTTP_REFERER'])) {
+    $roomId       = $_POST['roomId'];
+    $availability = $_POST['availability'];
 
-$sth = $dbh->prepare("UPDATE rooms SET availability=:availability WHERE roomId=$roomId");
-$sth->bindParam(':availability', $availability);
-$sth->execute();
+    $sth = $dbh->prepare("UPDATE rooms SET availability=:availability WHERE roomId=$roomId");
+    $sth->bindParam(':availability', $availability);
+    $sth->execute();
+}
