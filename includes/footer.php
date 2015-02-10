0e0f43122eec9baa08c0ceed22658296361ac1ad
JacobH1992 <ss_clan_cod@hotmail.com>
2015-02-10 14:09:29 +0000

    Added facebook, twitter, email icons to icon font

--- includes/footer.php
+++ includes/footer.php
@@ -2,12 +2,12 @@
     <div class="section-padding align-center">
         <div class="footer-block">
             <p>Find us on</p>
-            <a href="/" class="paragraph-a">Facebook</a>
-            <a href="/" class="paragraph-a">Twitter</a>
+            <i class="ico-facebook2"></i><a href="/" class="paragraph-a">Facebook</a>
+            <i class="ico-twitter"></i><a href="/" class="paragraph-a">Twitter</a>
         </div>
         <div class="footer-block">
             <p>Contact us</p>
-            <a href="mailto:info@studentrooma.co.uk" class="paragraph-a">info@studentrooma.co.uk</a>
+            <i class="ico-mail"></i><a href="mailto:info@studentrooma.co.uk" class="paragraph-a">info@studentrooma.co.uk</a>
         </div>
         <p class="copyright">&copy; Student Rooma 2015</p>
     </div>
--- views/index.php
+++ views/index.php
@@ -1,7 +1,9 @@
 <section class="lgrey bg">
     <div id="section-one" class="section-padding align-center">
         <h1 class="h1--show">Matching students to their perfect room!</h1>
-        <img src="/img/landing/phone1.png" alt="">
+        <div class="phone-image">
+            <img src="/img/landing/phone1.png" alt="">
+        </div>
         <a href="/app/"><button class="button-large">OPEN APP</button></a>
         <div class="landlord-link">
             <a href="/for-landlords">Are you a landlord?</a>
@@ -16,7 +18,9 @@
     <div id="section-two" class="section-padding align-center">
         <div class="text-right">
             <h2>Accomodation at the price, location and style you want.</h2>
-            <img src="/img/landing/phone2.png" alt="">
+            <div class="phone-image">
+                <img src="/img/landing/phone2.png" alt="">
+            </div>
             <p>This is student house window shopping from the comfort of the lecture hall, library or SU.</p>
             <ul>
                 <li>Use filters to find eactly what you want</li>
